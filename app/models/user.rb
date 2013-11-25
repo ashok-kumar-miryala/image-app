e9ca7ccc353502542375e84cdf2625741329f4f6
ashokkumarmiryala <ashokkumarmiryala@gmail.com>
2013-11-25 17:15:16 +0530

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  attr_accessible :name
+  attr_accessible :avatar
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageApp::Application.config.secret_token = '70d6c6dc37b10d09800d96ce0f4bfee483493f3ed3800341e9ac75298c382117de094f018e988a74be3b6293a61105332242491eb814d2f4dc1949c462cf4bcb'
